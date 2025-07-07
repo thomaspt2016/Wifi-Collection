412899f3964eb0d72aff1fc714f032ef539af04a
Thomas Pt <thomaspt2016@gmail.com>
2025-07-07 15:54:27 +0530

    brfore tweak

---
+++ common/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import CustomUser
+from django.contrib.auth.forms import UserCreationForm
+
+
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model=CustomUser
+        fields=['email','password1','password2','first_name','last_name']
