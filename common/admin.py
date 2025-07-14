0d320f985c4195d97e6d2c769bed274269741ea3
Thomas Pt <thomaspt2016@gmail.com>
2025-07-14 11:16:28 +0530

    admin.py update

--- common/admin.py
+++ common/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(CustomUser)
+admin.site.register(Building)
+admin.site.register(InternetPlan)
+admin.site.register(WifiCodeUpload)
+admin.site.register(CodePoool)
+admin.site.register(Billgeneration)
+admin.site.register(Payment)
+admin.site.register(Ticket)
+admin.site.register(Profile)
 
 # Register your models here.
